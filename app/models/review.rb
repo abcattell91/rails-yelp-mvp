1a4ba1acbf816d1fdd2cfb9bb0127cba043405ff
Alex Cattell <abcattell91@gmail.com>
2022-04-25 18:54:33 +0100

    Complete MVP Yelp review app

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning up database'
+Restaurant.destroy_all
+puts 'database is clean'
+
+puts 'Creating restaurants...'
+250.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "restaurant #{restaurant.id} is created"
+end
+
+puts 'done'
